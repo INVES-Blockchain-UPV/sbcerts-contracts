31452c062248f6fae441da320db4533134f6a283
Víctor Nicolás Martínez Carralero <57433047+Elpacos@users.noreply.github.com>
2022-10-16 18:15:10 +0200

    Update data structures BFactory.sol

--- contracts/BFactory.sol
+++ contracts/BFactory.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.8.9;
 
 contract BFactory {
 
-    uint256 id;
-    mapping(uint256 => address) addressCharla; ///id to address contract "charla"
-    mapping(string => uint256)  idCharla; /// name charla to id
+    uint256 currentEvent;
+    
+    mapping(uint256 => address) events; /// event id to Btoken.sol contract  
+    mapping(string => uint256)  eventsId; /// event name to id
 
     constructor() {
         
