d5a410f191873dd4335dd6f3e7d454d3866c1c30
RafaTatay <rafa.tatay.07@gmail.com>
2022-10-16 15:54:48 +0200

    Data models for BFactory

--- contracts/BFactory.sol
+++ contracts/BFactory.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.8.9;
 
 contract BFactory {
+
+    uint256 id;
+    mapping(uint256 => address) addressCharla; ///id to address contract "charla"
+    mapping(string => uint256)  idCharla; /// name charla to id
+
     constructor() {
         
     }
