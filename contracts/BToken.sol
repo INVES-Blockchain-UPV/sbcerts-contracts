c0ab15964dccae6910b907be081ef6285d469385
RafaTatay <rafa.tatay.07@gmail.com>
2022-10-16 16:07:23 +0200

    Data models for BToken

--- contracts/BToken.sol
+++ contracts/BToken.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.8.9;
 
 contract BToken {
+
+    uint256 startDate;
+    string tittle;
+    string description;
+    mapping(address => bool) holder; ///Token has been minted
     constructor() {
         
     }
