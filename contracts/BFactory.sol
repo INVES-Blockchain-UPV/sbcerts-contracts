80a7759658aa2d062d348f5557c884db3b564740
RafaTatay <rafa.tatay.07@gmail.com>
2022-10-18 20:52:51 +0200

    imported Ownable, completed constructor, function 'createEvent': arguments

--- contracts/BFactory.sol
+++ contracts/BFactory.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.8.9;
 
+import "./BToken.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 contract BFactory {
 
-    event createEvent(address indexed _event, uint256 _currentEvent, string _name);
+    event eventCreated(address indexed _event, uint256 _currentEvent, string _name);
     event removeEvent(address indexed _event, uint256 _eventid);
 
     uint256 currentEvent;
@@ -11,6 +14,18 @@ contract BFactory {
     mapping(string => uint256)  eventsId; /// event name to id
 
     constructor() {
-        
+        currentEvent = 1;
     }
+
+    
+    function createEvent(
+        address _events,
+        string memory _startDate,
+        uint256 _duration,
+        string memory _title,
+        string memory _description
+    ) public  {
+    
+    }  
+    
 }
